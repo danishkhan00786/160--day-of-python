16942fccf8f8681b1fa3f7cdaf24e75a713608c5
danish khan <danishbiss6@gmail.com>
2025-07-28 10:25:45 +0530

    Update Day05_pallindromechecker.py

--- Day05_pallindromechecker.py
+++ Day05_pallindromechecker.py
@@ -1,16 +1,7 @@
 expression = input("enter a expression").replace("","").lower()
 print(f"your given expression ({expression}) now checking for pallindrome")
-reverse_expression = []
-for i in range(len(expression)-1,-1,-1):
- reverse_expression.append(expression[i])
-i = 0
-is_pallindrome = True
-while i<len(reverse_expression):
- if reverse_expression[i] != expression[i] :
-    is_pallindrome = False
-break
-i+=1
-if is_pallindrome:
+
+if expression == expression[::-1]:
   print("✅ Yes, given expression is pallindrome")
 else :
   print("❌not pallindrome")
